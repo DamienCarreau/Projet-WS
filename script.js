a24551d5d0528b897fbe69f33319e46091bb3b3b
DamienCarreau <verybadchti@gmail.com>
2020-11-06 11:53:44 +0100

    Categories

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-/**
- * Récupération de données depuis DBPedia dès qu'on clique sur le bouton
- * Puis affichage des données récupérées sur la page
- */
-document.getElementById("target").onclick = function() {
-	var uri = "http://dbpedia.org/ontology/Food";
+$('document').ready(function () {
+  var search;
+  var res = null,
+  tmp = [];
+  location.search
+          .substr(1)
+          .split("&")
+          .forEach(function (item) {
+              tmp = item.split("=");
+              if (tmp[0] === "request")
+                  res = decodeURIComponent(tmp[1]);
+          });
+  if (res === null) {
+      alert("La recherche n'a pas été spécifié");
+      return;
+  }
 
-  var value = document.getElementById("req").value;
+  var uri = "http://dbpedia.org/ontology/Food";
 
   //La requête SPARQL à proprement parler
-	var querySPARQL=""+
-    'PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> \n'+
-    'SELECT ?l WHERE {\n'+
-    '    ?c a dbpedia-owl:Food.\n'+
-    '    ?c rdfs:label ?l.\n'+
-    'FILTER (lang(?l) = "fr"';
-    if(value.length !== 0)
-      querySPARQL += '&& regex(?l,"'+document.getElementById("req").value+'","i")';
-    querySPARQL += ')}';
+  var querySPARQL=""+
+    'SELECT ?property ?isValueOf\n'+
+    'WHERE {\n'+
+    '    { ?isValueOf ?property <http://dbpedia.org/resource/Category:'+res+'> }\n'+
+    '}';
 
   // On prépare l'URL racine (aussi appelé ENDPOINT) pour interroger DBPedia (ici en français)
   var baseURL="http://dbpedia.org/sparql";
@@ -26,22 +32,28 @@ document.getElementById("target").onclick = function() {
 
   //On crée notre requête AJAX
   var req = new XMLHttpRequest();
-	req.open("GET", queryURL, true);
-	req.onreadystatechange = myCode;   // the handler
-	req.send(null);        
+  req.open("GET", queryURL, true);
+  req.onreadystatechange = myCode;   // the handler
+  req.send(null);        
 
-	function myCode() {
-	   if (req.readyState == 4) {
+  function myCode() {
+     if (req.readyState == 4) {
         document.getElementById("result").innerHTML = "";
-	      var doc = JSON.parse(req.responseText);
+        var doc = JSON.parse(req.responseText);
 
         var div = $("#result");
         $.each(doc.results.bindings,
           function (index, element) {
-            div.append($('<div>',{
-              "text": (element.l.value)
-            }));
+            if((element.property.value).search("broader") !== -1){
+              var d = $('<div>');
+              d.append($('<a>',{
+                "text": (element.isValueOf.value).substr(37),
+                "href": "?request="+(element.isValueOf.value).substr(37)
+              }));
+              div.append(d);
+            }
+            
           });
-	   }
-	}
-};
+     }
+  }
+});
