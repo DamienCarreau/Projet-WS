ce25c0d50a2d699b8cc50d38ce27bff1d13e6e45
Doreapp <55652808+Doreapp@users.noreply.github.com>
2020-11-08 11:06:13 +0100

    Ajout des fonctions sur l'aliment
    L'ensemble des fonctions pour query l'ensemble des informations sur les aliments

--- js/aliment.js
+++ js/aliment.js
@@ -1,5 +1,7 @@
-var uri = 'http://dbpedia.org/resource/Fruit';
+//TODO change the way of specifying searched data
+// data = new URL(document.location.href).searchParams.get("data");
 
+// var uri = 'http://dbpedia.org/resource/' + data;
 
 var basePrefixes = "" +
     'PREFIX dbo: <http://dbpedia.org/ontology/> ' +
@@ -10,66 +12,398 @@ var basePrefixes = "" +
     'PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> ' +
     'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ';
 
-function findInformation() {
-    //La requête SPARQL à proprement parler
+/*
+STRUCTURE DU RESULTAT : 
+generalInformations 
+    {
+        + label
+        + abstract
+        ? alias 
+        ? thumbnail
+        + descendingTypes [
+            + label
+            + link
+        ]
+    }
+composition 
+    {
+        ? betacaroteneUg
+        ? calciumMg
+        ? calories
+        ? carbs
+        ? fat
+        ? fiber
+        ? ironMg
+        ? kj
+        ? magnesiumMg
+        ? phosphorusMg
+        ? protein
+        ? sodiumMg
+        ? sugars
+        ? vitaUg
+        ? vitb6Mg
+        ? vitcMg
+        ? viteMg
+        ? vitkUg
+        ? water
+        ? zincMg
+    }
+origin 
+    {
+        ? region (Jamais vue)
+        + countries [
+            + label
+            ? link
+        }
+        + productors [
+            + label
+            + link
+        ]
+    }
+usage 
+    {
+        ? servingSize
+        ? servingTemperature
+        + receipes [
+            + label
+            + link
+        ]
+        + ingredients [
+            + label
+            + link
+        ]
+    }
+*/
 
-    //(GROUP_CONCAT(?personName;SEPARATOR=",") AS ?list)
-    var querySPARQL1 = basePrefixes +
+/**
+ * findFoodInformations
+ * Retourne l'ensemble des informations sur le produit (en tant que nourriture)
+ * @param {String} uri URI du produit en question 
+ * @param {function(JsonObject)} onResult fonction appelée lorsque le résultat est trouvé 
+ */
+function findFoodInformations(uri, onResult) {
+    var count = 0;
+    var totalCount = 4;
+    var finalResult = {};
+
+    findGeneralInformations(uri, function(result) {
+        count += 1;
+        finalResult.generalInformations = result;
+        if (count == totalCount) {
+            onResult(finalResult);
+        }
+    });
+
+
+    findComposition(uri, function(result) {
+        count += 1;
+        finalResult.composition = result;
+        if (count == totalCount) {
+            onResult(finalResult);
+        }
+    });
+
+
+    findOrigin(uri, function(result) {
+        count += 1;
+        finalResult.origin = result;
+        if (count == totalCount) {
+            onResult(finalResult);
+        }
+    });
+
+
+    findUsage(uri, function(result) {
+        count += 1;
+        finalResult.usage = result;
+        if (count == totalCount) {
+            onResult(finalResult);
+        }
+    });
+}
+
+/**
+ * findGeneralInformations
+ * Trouve les informations générales sur le produits
+ *  {
+ *      + label
+ *      + abstract
+ *      ? alias 
+ *      ? thumbnail
+ *      + descendingTypes [
+ *          + label
+ *          + link
+ *      ]
+ * }
+ * @param {String} uri URI de l'objet en question
+ * @param {function(JsonObject)} onResult fonction appelé à la fin de la requete.
+ */
+function findGeneralInformations(uri, onResult) {
+    var queryBasics = basePrefixes +
         'SELECT ' +
         '?label ' +
+        '?alias ' +
         '?abstract ' +
         '?thumbnail ' +
-        '(GROUP_CONCAT(?productorName;SEPARATOR=",") AS ?productors) ' +
-
         'WHERE { ' +
         '  <' + uri + '> rdfs:label ?label . ' +
         '  <' + uri + '> dbo:abstract ?abstract . ' +
         '  OPTIONAL { ' +
         '    <' + uri + '> dbo:thumbnail ?thumbnail . ' +
-        '    <' + uri + '> dbo:product ?productor . ' +
-        '    ?productor dbo:product ?productorName . ' +
+        '  } ' +
+        '  OPTIONAL { ' +
+        '    <' + uri + '> dbo:alias ?alias . ' +
+        '    FILTER(lang(?alias) = "en") ' +
         '  } ' +
         '  FILTER(lang(?abstract) = "en" && lang(?label) = "en")' +
         '} ' +
-        'LIMIT 100';
+        'LIMIT 1';
 
-    queryData(querySPARQL1, function(results) {
-        console.log(results);
-        var resPlace = document.getElementById("result_place");
-        resPlace.innerText = JSON.stringify(results);
+    var queryTypeOf = basePrefixes +
+        'SELECT ' +
+        '?label ' +
+        '?link ' +
+        'WHERE { ' +
+        '  ?link dbo:type <' + uri + '> . ' +
+        '  ?link rdfs:label ?label . ' +
+        '  FILTER(lang(?label) = "en")' +
+        '} ' +
+        'LIMIT 50';
+
+    var finalResult = null;
+    queryData(queryBasics, function(result) {
+        // on Result return the array of results or we only want 
+        // the first row.
+        finalResult = result[0];
+        if (finalResult == undefined) {
+            finalResult = {};
+        }
+        queryData(queryTypeOf, function(result) {
+            finalResult.descendingTypes = result;
+            onResult(finalResult);
+        });
+    });
+}
+
+/**
+ * findComposition
+ * Trouve les informations sur la composition / les valeurs nutritives
+ *  {
+ *      ? betacaroteneUg
+ *      ? ... (voir sur docs)
+ * }
+ * @param {String} uri URI de l'objet en question
+ * @param {function(JsonObject)} onResult fonction appelé à la fin de la requete.
+ */
+function findComposition(uri, onResult) {
+    var queryCompposition = basePrefixes +
+        'SELECT * ' +
+        'WHERE { ' +
+        '  OPTIONAL { <' + uri + '> dbp:betacaroteneUg ?betacaroteneUg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:calciumMg ?calciumMg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:calories ?calories . }' +
+        '  OPTIONAL { <' + uri + '> dbp:carbs ?carbs . }' +
+        '  OPTIONAL { <' + uri + '> dbp:fat ?fat . }' +
+        '  OPTIONAL { <' + uri + '> dbp:fiber ?fiber . }' +
+        '  OPTIONAL { <' + uri + '> dbp:ironMg ?ironMg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:kj ?kj . }' +
+        '  OPTIONAL { <' + uri + '> dbp:magnesiumMg ?magnesiumMg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:phosphorusMg ?phosphorusMg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:sodiumMg ?sodiumMg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:sugars ?sugars . }' +
+        '  OPTIONAL { <' + uri + '> dbp:vitaUg ?vitaUg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:vitb6Mg ?vitb6Mg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:vitcMg ?vitcMg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:viteMg ?viteMg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:vitkUg ?vitkUg . }' +
+        '  OPTIONAL { <' + uri + '> dbp:water ?water . }' +
+        '  OPTIONAL { <' + uri + '> dbp:zincMg ?zincMg . }' +
+        '} ' +
+        'LIMIT 1';
+
+    queryData(queryCompposition, function(result) {
+        // on Result return the array of results or we only want 
+        // the first row.
+        onResult(result[0]);
     })
 }
 
 /**
- * findReceipes Trouve 10 recettes dans lesquelles l'ingrédient est.
- * @param {int} offset indice du premier ingrédient, 0 par défaut
+ * findOrigin
+ * Trouve les informations sur l'origine du prodtui
+ * {
+ *      ? region (Jamais vue)
+ *      + countries [
+ *          + label
+ *          ? link
+ *      }
+ *      + productors [
+ *          + label
+ *          + link
+ *      ]
+ * }
+ * @param {String} uri URI de l'objet en question
+ * @param {function(JsonObject)} onResult fonction appelée à l'arrivée du resultat 
  */
-function findReceipes(offset = 0) {
-    //La requête SPARQL à proprement parler
-    var querySPARQL1 = basePrefixes +
+function findOrigin(uri, onResult) {
+
+    var queryRegion = basePrefixes +
+        'SELECT * ' +
+        'WHERE { ' +
+        '  <' + uri + '> dbo:region ?region . ' +
+        '} ' +
+        'LIMIT 1';
+
+
+    var queryCountries = basePrefixes +
+        'SELECT * ' +
+        'WHERE { ' +
+        ' { ' +
+        '  <' + uri + '> dbo:country ?link . ' +
+        '  ?link rdfs:label ?label ' +
+        '  FILTER(lang(?label) = "en") ' +
+        ' } UNION { ' +
+        '  <' + uri + '> dbp:country ?label ' +
+        ' } ' +
+        '} ' +
+        'LIMIT 50';
+
+
+    var queryProductors = basePrefixes +
+        'SELECT * ' +
+        'WHERE { ' +
+        '  ?link dbo:product <' + uri + '> . ' +
+        '  ?link rdfs:label ?label . ' +
+        '  FILTER(lang(?label) = "en") ' +
+        '} ' +
+        'LIMIT 50';
+
+    var finalResult = null;
+    queryData(queryRegion, function(result) {
+        // on Result return the array of results or we only want 
+        // the first row.
+        finalResult = result[0];
+        if (finalResult == undefined) {
+            finalResult = {};
+        }
+        queryData(queryCountries, function(result) {
+            finalResult.countries = result;
+
+            queryData(queryProductors, function(result) {
+                finalResult.productors = result;
+                onResult(finalResult);
+            });
+        });
+    });
+}
+
+/**
+ * findUsage
+ * Trouve les "usages" du produit
+ * {
+ *      ? servingSize
+ *      ? servingTemperature
+ *      + receipes [
+ *          + label
+ *          + link
+ *      ]
+ *      + ingredients [
+ *          + label
+ *          + link
+ *      ]
+ * }
+ * @param {String} uri URI de l'objet en question
+ * @param {function(JsonObject)} onResult fonction appelée à l'arrivée du resultat 
+ */
+function findUsage(uri, onResult) {
+    var queryServingData = basePrefixes +
+        'SELECT * ' +
+        'WHERE { ' +
+        '  OPTIONAL { <' + uri + '> dbo:servingSize ?servingSize . } ' +
+        '  OPTIONAL { <' + uri + '> dbo:servingTemperature ?servingTemperature . } ' +
+        '} ' +
+        'LIMIT 1';
+
+    var finalResult = null;
+    queryData(queryServingData, function(result) {
+        // on Result return the array of results or we only want 
+        // the first row.
+        finalResult = result[0];
+        if (finalResult == undefined) {
+            finalResult = {};
+        }
+        findReceipes(uri, function(result) {
+            finalResult.receipes = result;
+            findIngredients(uri, function(result) {
+                finalResult.ingredients = result;
+                onResult(finalResult);
+            });
+        });
+    });
+}
+
+/**
+ * findReceipes
+ * Trouve les recettes qui utilisent le produit
+ * [
+ *      + label
+ *      + link
+ * ]
+ * @param {String} uri URI de l'objet en question
+ * @param {function(JsonObject)} onResult fonction appelé au résultat 
+ * @param {Integer} offset (Optionnel) Indice de la première recette à trouver (0 par défaut)
+ * @param {Integer} limit (Optionnel) Nombre maximal de recettes à retourner (20 par défaut)
+ */
+function findReceipes(uri, onResult, offset = 0, limit = 20) {
+    var queryReceipes = basePrefixes +
         'SELECT ' +
-        '?label ' +
-        '?receipe ' +
+        ' * ' +
 
         'WHERE { ' +
-        '  ?receipe dbo:ingredient <' + uri + '> . ' +
-        '  ?receipe rdfs:label ?label . ' +
+        '  ?link dbo:ingredient <' + uri + '> . ' +
+        '  ?link rdfs:label ?label . ' +
         '  FILTER(lang(?label) = "en")' +
         '} ' +
         'OFFSET ' + offset + ' ' +
-        'LIMIT 10';
+        'LIMIT ' + limit;
 
-    queryData(querySPARQL1, function(results) {
-        console.log(results);
-        var resPlace = document.getElementById("result_place");
-        resPlace.innerText = JSON.stringify(results);
-    })
+    queryData(queryReceipes, onResult);
+
 }
+
+/**
+ * findIngredients
+ * Trouve les ingrédients du produit
+ * [
+ *      + label
+ *      + link
+ * ]
+ * @param {String} uri URI de l'objet en question
+ * @param {function(JsonObject)} onResult fonction appelé au résultat 
+ * @param {Integer} offset (Optionnel) Indice du premier ingrédient à trouver (0 par défaut)
+ * @param {Integer} limit (Optionnel) Nombre maximal d'ingrédients à retourner (20 par défaut)
+ */
+function findIngredients(uri, onResult, offset = 0, limit = 20) {
+    var queryIngredients = basePrefixes +
+        'SELECT ' +
+        ' * ' +
+
+        'WHERE { ' +
+        '  <' + uri + '> dbo:ingredient  ?link . ' +
+        '  ?link rdfs:label ?label . ' +
+        '  FILTER(lang(?label) = "en")' +
+        '} ' +
+        'OFFSET ' + offset + ' ' +
+        'LIMIT ' + limit;
+
+    queryData(queryIngredients, onResult);
+}
+
 
 /**
  * queryData
  * @param {String} querySPARQL la query SparQL  
- * @param {function(JsonObject)} onResult fonction appelée lorsque le résultat arrive 
+ * @param {function(JsonArray)} onResult fonction appelée lorsque le résultat arrive 
  */
 function queryData(querySPARQL, onResult) {
     //On prépare l'URL racine (aussi appelé ENDPOINT) pour interroger DBPedia (ici en français)
@@ -96,4 +430,6 @@ function queryData(querySPARQL, onResult) {
     }
 }
 
-console.log("end of file aliment");
+function goToObject(object) {
+    document.location.href = "./index.html?data=" + object;
+}
